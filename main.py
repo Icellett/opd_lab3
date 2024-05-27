d979bf191f0135d16cad892264fe6d75beb5ee15
Icellett <anya.sankova.05@gmail.com>
2024-05-28 03:53:40 +0600

    FixFinal

--- main.py
+++ main.py
@@ -36,34 +36,34 @@ def toPage():
 
 @app.route('/paral', methods=['post', 'get'])
 def paralVolume():
-    num_1 = int(request.form.get('num_1'))
-    num_2 = int(request.form.get('num_2'))
-    num_3 = int(request.form.get('num_3'))
+    num_1 = float(request.form.get('num_1'))
+    num_2 = float(request.form.get('num_2'))
+    num_3 = float(request.form.get('num_3'))
     e = int(request.form.get('e'))
     res = round((num_1*num_2*num_3), e)
     return render_template('paral.html', res=res)
 
 @app.route('/sphere', methods=['post', 'get'])
 def sphereVolume():
-    num_1 = int(request.form.get('num_1'))
+    num_1 = float(request.form.get('num_1'))
     e = int(request.form.get('e'))
     res = round((4 / 3) * math.pi * (num_1 ** 3), e)
     return render_template('sphere.html', res=res)
 
 @app.route('/cone', methods=['post', 'get'])
 def coneVolume():
-    num_1 = int(request.form.get('num_1'))
-    num_2 = int(request.form.get('num_2'))
+    num_1 = float(request.form.get('num_1'))
+    num_2 = float(request.form.get('num_2'))
     e = int(request.form.get('e'))
-    res = round((1 / 3) * math.pi * (num_1 ** 2), e)
+    res = round((1 / 3) * math.pi * (num_1 ** 2) * num_2, e)
     return render_template('cone.html', res=res)
 
 @app.route('/cylinder', methods=['post', 'get'])
 def cylinderVolume():
-    num_1 = int(request.form.get('num_1'))
-    num_2 = int(request.form.get('num_2'))
+    num_1 = float(request.form.get('num_1'))
+    num_2 = float(request.form.get('num_2'))
     e = int(request.form.get('e'))
-    res = round(math.pi * (num_1 ** 2), e)
+    res = round(math.pi * (num_1 ** 2) * num_2, e)
     return render_template('cylinder.html', res=res)
 
 if __name__ == "__main__":
